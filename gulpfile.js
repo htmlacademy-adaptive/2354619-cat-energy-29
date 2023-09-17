e0fde1d0a456cdca4fb6112f5c657eff3109ee09
Viktoria <toz.vito@gmail.com>
2023-09-17 03:13:26 +0300

    Добавляет завершенный каталог и автоматизацию сборки

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,14 @@ import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 import svgo from 'gulp-svgmin';
-import svgstore from 'gulp-svgstore';
 import { stacksvg } from "gulp-stacksvg";
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import { deleteAsync as del } from 'del';
+
 // Styles
 
 export const styles = () => {
@@ -14,36 +20,100 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
 
-export const stackSvg = () => {
-  return gulp.src('source/img/sprite-source/*.svg')
-    .pipe(svgo({
-      plugins: [
-        {
-          removeUnknownsAndDefaults: false
-        }
-      ]
-    }))
-    .pipe(stacksvg({ output: `sprite` }))
-    .pipe(gulp.dest('build/img'));
+// HTML
+
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
 }
+
+// Scripts
+
+export const scripts = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+}
+
+// Images
+
+const optimizeImages = () => {
+  return gulp.src('source/img/*.{jpg,png}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'))
+}
+
+export const copyImages = () => {
+  return gulp.src('source/img/*.{jpg,png}')
+    .pipe(gulp.dest('build/img'))
+}
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src('source/img/*.{jpg,png}')
+    .pipe(squoosh({
+      webp: {}
+    }))
+    .pipe(gulp.dest('build/img'))
+}
+
+// SVG
+
+const svgStack = () => {
+  return gulp.src('source/img/sprite-source/*.svg')
+    .pipe(svgo())
+    .pipe(stacksvg({ output: `sprite` }))
+    .pipe(gulp.dest('build/img/svg/'))
+}
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    'source/fonts/**/*.{woff2,woff}',
+    'source/img/all-svg/*.svg'
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'))
+  done();
+}
+
+// Clean
+
+export const clean = () => {
+  return del('build');
+};
 
 // Server
 
-const server = (done) => {
+function server(done) {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
     ui: false,
+    browser: "opera"
   });
+  done();
+}
+
+// Reload
+
+const reload = (done) => {
+  browser.reload();
   done();
 }
 
@@ -51,12 +121,39 @@ const server = (done) => {
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/js/*.js', gulp.series(scripts));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
+// Build
 
-export default gulp.series(
-  styles, stacksvg, server, watcher
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    svgStack,
+    createWebp
+  ),
 );
 
+// Default
 
+export default gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    svgStack,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
